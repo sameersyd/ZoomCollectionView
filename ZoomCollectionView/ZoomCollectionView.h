031d89241641f762d6f7bc6863f12da717c6b421
Sameer Nawaz <sameer.nwaz@gmail.com>
2020-08-03 23:45:57 +0530

    Initial Commit

---
+++ ZoomCollectionView/ZoomCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  ZoomCollectionView.h
+//  ZoomCollectionView
+//
+//  Created by Sameer Nawaz on 03/08/20.
+//  Copyright © 2020 Femargent Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZoomCollectionView.
+FOUNDATION_EXPORT double ZoomCollectionViewVersionNumber;
+
+//! Project version string for ZoomCollectionView.
+FOUNDATION_EXPORT const unsigned char ZoomCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomCollectionView/PublicHeader.h>
+
+
