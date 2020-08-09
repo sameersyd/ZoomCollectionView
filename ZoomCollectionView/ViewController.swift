30158109f35b52bf6d8da15fa80e03f27d2e8290
Sameer Nawaz <sameer.nwaz@gmail.com>
2020-08-10 01:44:42 +0530

    open class

--- ZoomCollectionView/ViewController.swift
+++ ZoomCollectionView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UPCarouselFlowLayout
 
-public class ViewController: UIViewController {
+open class ViewController: UIViewController {
     
     @IBOutlet weak var collView: UICollectionView!
     
